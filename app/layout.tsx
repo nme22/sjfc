f93087393a4d22e07fac1214541609455ed9f2cd
Nav Ebrahimi <nav@clerk.dev>
2024-12-06 00:36:31 -0500

    yeah we need that back...

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 
 export const metadata: Metadata = {
