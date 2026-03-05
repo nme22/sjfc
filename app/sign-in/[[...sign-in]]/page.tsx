597ef8474502bb7c254d55b60ffde10eaff65466
Nav Ebrahimi <nav@clerk.dev>
2026-03-05 05:48:07 -0500

    Fix sign-in page not visible on mobile by adding proper container height

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center items-center min-h-[calc(100vh-64px)] py-8'>
       <SignIn
         withSignUp={true}
         unsafeMetadata={{ position: ['Center Midfielder'] }}
